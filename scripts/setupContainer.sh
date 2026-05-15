70b9d5b4a53c9c78621ec5422a0b504722dec5df
FunMetJoel <machinebouwclub@gmail.com>
2026-05-15 10:16:53 +0200

    fixed setup script

--- scripts/setupContainer.sh
+++ scripts/setupContainer.sh
@@ -6,6 +6,6 @@ docker run --rm -it --name turtlebot3_container \
  -v /home/c2irr10/turtlebot3_ws:/ws \
  --user $(id -u):$(id -g) \
  turtlebot3_ws \
- bash -c "source /opt/ros/jazzy/setup.bash && /opt/turtlebot3_ws/install/setup.bash && source install/setup.bash && export TURTLEBOT3_MODEL=burger && ros2 launch my_tb3_world new_world.launch.py && exec bash"
+ bash -c "source /opt/ros/jazzy/setup.bash && source /opt/turtlebot3_ws/install/setup.bash && source install/setup.bash && export TURTLEBOT3_MODEL=burger && ros2 launch my_tb3_world new_world.launch.py && exec bash"
 
 
