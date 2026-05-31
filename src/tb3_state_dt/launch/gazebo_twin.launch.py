834862f22edcae7df57a58e6d750e81fd2a8b15d
FunMetJoel <machinebouwclub@gmail.com>
2026-05-31 23:18:09 +0200

    Added the Gazebo digital twin

---
+++ src/tb3_state_dt/launch/gazebo_twin.launch.py
@@ -0,0 +1,66 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, GroupAction
+from launch_ros.actions import Node, PushRosNamespace
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
+from launch.actions import AppendEnvironmentVariable
+
+
+def generate_launch_description():
+        
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+
+    world_path = PathJoinSubstitution([
+        FindPackageShare('tb3_state_dt'),
+        'worlds',
+        'sim_world.world'
+    ])
+
+    set_env_vars_resources  = AppendEnvironmentVariable('GZ_SIM_RESOURCE_PATH', os.path.join(
+                get_package_share_directory('turtlebot3_gazebo'),
+                'models'))
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                    FindPackageShare('ros_gz_sim'),
+                    'launch',
+                    'gz_sim.launch.py'
+            ])
+        ),
+        launch_arguments={
+            'gz_args': ['-r -v2 ', world_path],
+        }.items()
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')),
+        launch_arguments={'-topic': '/sim/robot_description', '-name': 'turtlebot3', 'x_pose': '0', 'y_pose': '0'}.items()
+    )
+
+    
+    twin_simulation_group = GroupAction([
+        PushRosNamespace('sim'),
+        SetEnvironmentVariable(name='GZ_PARTITION', value='digital_twin_partition'),
+        gazebo,
+        robot_state_publisher_cmd,
+        spawn_turtlebot_cmd,
+        set_env_vars_resources
+    ])
+
+    return LaunchDescription([
+        twin_simulation_group
+    ])
